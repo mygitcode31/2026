6ba8f1c19805b3e1318eafdf0cbdd1bd8385775a
Naresh Prasad Yadav <npy.personal@gmail.com>
2026-05-16 20:12:35 +0530

    Update loop-script.sh

--- Bash/loop-script.sh
+++ Bash/loop-script.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-a=4
-b=10
-if [ $a -gt $b ]
+
+
+#########################
+## Author: Naresh
+## Date: 16-05-2026
+## Purpose: divisible by 3,5 but not by 15
+## Version: v1
+
+
+for i in {1..100}; do
+if ([ `expr $i % 3` == 0 ] || [ `expr $i % 5` == 0 ]) && [ `expr $i % 15` != 0 ];
 then
-  echo "a is greater than b"
-else
-  echo "b is greater than a"
-fi
-
-
-# -gt, -lt, -eq, -ne, -ge, -le,
-# These only work with numbers. For strings you use == and != inside [ ].
+  echo $i;
+fi;
+done
